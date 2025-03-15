29eeab762234e9fcc0d4f54773cbc996883ba809
miaviles11 <ma.aviles.garcia@gmail.com>
2025-03-15 16:30:37 +0100

    Added structures, implemented input parsing function

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: miaviles <miaviles@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 19:19:16 by miaviles          #+#    #+#             */
-/*   Updated: 2025/03/13 17:45:21 by miaviles         ###   ########.fr       */
+/*   Updated: 2025/03/15 15:39:20 by miaviles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,55 @@
 # include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+/*
+** t_pipe: Estructura para el manejo de pipes.
+** Se utiliza para almacenar dos conjuntos de descriptores (para alternar entre ellos),
+** además de las copias de los descriptores estándar (in, out) y flags de estado.
+*/
+typedef struct s_pipe
+{
+	int	fd[2][2];
+	int	in;
+	int	out;
+	int	last;
+	int	out_error;
+}				t_pipe;
+
+/*
+** t_cmd: Nodo de comando.
+** Cada nodo contiene el nombre del comando, su arreglo de argumentos y
+** el número de argumentos. La información global (entorno, flags, pipes)
+** se encuentra en la estructura principal y se pasa como argumento a las funciones.
+*/
+typedef struct s_cmd
+{
+	struct s_cmd	*next;
+	char			*cmd;
+	char			**arg;
+	int				num_arg;
+}				t_cmd;
+
+/*
+** t_msh: Estructura principal del shell.
+** Se integran aquí tanto el entorno como las banderas globales, ya que estos
+** datos se usan en toda la ejecución y son comunes a todos los comandos.
+*/
+typedef struct s_msh
+{
+	int		bash_lvl;
+	int		total_chars;
+	t_cmd	*cmd;
+	char	**env;      // Array de variables de entorno (cada cadena "VAR=VAL")
+	char	*path;      // Contenido de la variable PATH, extraído del env
+	int		num_env;    // Número de variables en env
+	/* Banderas globales */
+	int		quote;          // Indica si hay error en el balance de comillas
+	int		pipe;           // Número de pipes detectados en la línea
+	int		redic;          // Indicador de redirecciones
+	int		dollar_special; // Indicador para casos especiales con $
+}				t_msh;
+
 
 int	is_line_empty(char *s);
 int is_builtin_no_pipeline(t_cmd *cmd, char *cmd_name);
--- include/parser.h
+++ include/parser.h
@@ -6,23 +6,11 @@
 /*   By: miaviles <miaviles@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 12:41:28 by miaviles          #+#    #+#             */
-/*   Updated: 2025/03/15 12:41:28 by miaviles         ###   ########.fr       */
+/*   Updated: 2025/03/15 15:39:04 by miaviles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
-
-typedef struct s_cmd
-{
-    char    *cmd;         // Nombre del comando
-    char    **args;       // Argumentos
-    char    *infile;      // Archivo de entrada ('<' o '<<')
-    char    *outfile;     // Archivo de salida ('>' o '>>')
-    int     append;       // Flag para '>>' (modo append)
-    int     heredoc;      // Flag para '<<'
-    int     pipe;         // Flag para '|'
-    struct s_cmd *next;   // Siguiente comando en pipeline
-} t_cmd;
 
 #endif
