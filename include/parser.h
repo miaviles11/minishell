e218f12e4627a825a4bceee5bda23f003527598c
miaviles11 <ma.aviles.garcia@gmail.com>
2025-03-15 18:26:15 +0100

    añadidas funciones a la carpeta de parseo

--- include/parser.h
+++ include/parser.h
@@ -6,11 +6,16 @@
 /*   By: miaviles <miaviles@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 12:41:28 by miaviles          #+#    #+#             */
-/*   Updated: 2025/03/15 15:39:04 by miaviles         ###   ########.fr       */
+/*   Updated: 2025/03/15 18:12:52 by miaviles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
+
+int	parse_input_line(t_msh *shell, t_cmd **commandList, char *inputLine);
+int	validate_and_split_input(t_msh *shell, char *inputLine, char ***segments);
+int	check_quotes_balance(const char *s, t_msh *shell);
+static int	both_quotes_exist(const char *s, int *i, char quote);
 
 #endif
---
+++ src/parser/utils_parser.c
@@ -0,0 +1,81 @@
+int	validate_and_split_input(t_msh *shell, char *inputLine, char ***segments)
+{
+	/* 1. Verifica que las comillas estén balanceadas.
+	   check_quotes_balance retorna un valor distinto de 0 si están balanceadas.
+	*/
+	shell->quote = check_quotes_balance(inputLine, shell);
+	if (shell->quote == 0)
+	{
+		free(inputLine);
+		return (1);
+	}
+	/* 2. Cuenta el número de pipes en la línea.
+	   count_pipes retorna -1 si hay error de sintaxis (por ejemplo, pipe mal colocado).
+	*/
+	shell->pipe = count_pipes(inputLine);
+	if (shell->pipe == -1)
+	{
+		free(inputLine);
+		return (1);
+	}
+	/* 3. Divide la línea en segmentos utilizando el pipe ('|') como delimitador.
+	   split_pipes devuelve un arreglo de cadenas (cada una es un segmento).
+	*/
+	*segments = split_pipes(inputLine);
+	if (!(*segments))
+	{
+		free(inputLine);
+		return (1);
+	}
+	/* Si todo es correcto, retorna 0 indicando que no hubo errores */
+	return (0);
+}
+/*
+** check_quotes_balance:
+**   Verifica que en la cadena 's' las comillas dobles y simples estén balanceadas.
+**   Retorna 1 si están balanceadas; de lo contrario, retorna 0.
+**   Además, en caso de error, actualiza el campo 'quote' de la estructura shell.
+**
+** Nota: Se utiliza la función auxiliar both_quotes_exist() para procesar
+**       la apertura/cierre de cada tipo de comilla.
+*/
+int	check_quotes_balance(const char *s, t_msh *shell)
+{
+	int	i;
+	int	simple_count;
+	int	double_count;
+
+	i = 0;
+	simple_count = 0;
+	double_count = 0;
+	while (s[i])
+	{
+		if (s[i] == '"' )
+			double_count += both_quotes_exist(s, &i, '"');
+		else if (s[i] == '\'')
+			simple_count += both_quotes_exist(s, &i, '\'');
+		// Si alguna cuenta es impar, las comillas no están balanceadas.
+		if ((double_count % 2) != 0 || (simple_count % 2) != 0)
+		{
+			shell->quote = 1;
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+static int	both_quotes_exist(const char *s, int *i, char quote)
+{
+	int	pairing;
+
+	pairing = 1;
+	// Avanza mientras haya caracteres y no se encuentre la comilla de cierre.
+	while (s[*i + 1] && s[*i + 1] != quote)
+		(*i)++;
+	// Si se encontró la comilla de cierre, incrementa el contador.
+	if (s[*i + 1] == quote)
+		pairing++;
+	// Salta la comilla de cierre (o el final si no se encontró).
+	(*i)++;
+	return (pairing);
+}
