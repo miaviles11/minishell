6a988f428d55e08e79409cc6c3e4f0589e36bba3
miaviles11 <ma.aviles.garcia@gmail.com>
2025-05-10 12:32:52 +0200

    signals, and half of redirections passed the norminette, headers optimizated

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -5,72 +5,71 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: miaviles <miaviles@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/18 18:29:26 by miaviles          #+#    #+#             */
-/*   Updated: 2025/04/20 19:34:39 by carlsanc         ###   ########.fr       */
+/*   Created: 2025/05/10 12:31:03 by miaviles          #+#    #+#             */
+/*   Updated: 2025/05/10 12:31:03 by miaviles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-extern volatile sig_atomic_t g_interactive;
+extern volatile sig_atomic_t	g_interactive;
 
-void handle_parent_signal(int sign)
+void	handle_parent_signal(int sign)
 {
-    if (sign == SIGINT)
-    {
-        write(STDOUT_FILENO, "\n", 1);
-        if (g_interactive)
-        {
-            rl_on_new_line();
-            rl_replace_line("", 0);
-            rl_redisplay();
-        }
-    }
-    else if (sign == SIGQUIT)
-    {
-        if (!g_interactive)
-        {
-            write(STDERR_FILENO, "Quit: 3\n", 8);
-        }
-        else
-        {
-            rl_on_new_line();
-            rl_replace_line("", 0);
-            rl_redisplay();
-        }
-    }
+	if (sign == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		if (g_interactive)
+		{
+			rl_on_new_line();
+			rl_replace_line("", 0);
+			rl_redisplay();
+		}
+	}
+	else if (sign == SIGQUIT)
+	{
+		if (!g_interactive)
+		{
+			write(STDERR_FILENO, "Quit: 3\n", 8);
+		}
+		else
+		{
+			rl_on_new_line();
+			rl_replace_line("", 0);
+			rl_redisplay();
+		}
+	}
 }
 
-void handle_child_signal(int sign)
+void	handle_child_signal(int sign)
 {
-    if (sign == SIGINT)
-        _exit(130);
-    else if (sign == SIGQUIT)
-        _exit(131);
+	if (sign == SIGINT)
+		_exit(130);
+	else if (sign == SIGQUIT)
+		_exit(131);
 }
 
-void setup_signals(void)
+void	setup_signals(void)
 {
-    struct sigaction sa;
+	struct sigaction	sa;
 
-    sa.sa_handler = handle_parent_signal;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    sigaction(SIGINT,  &sa, NULL);
-    sigaction(SIGQUIT, &sa, NULL);
-
-    sa.sa_handler = SIG_IGN;
-    sigaction(SIGTERM, &sa, NULL);
+	sa.sa_handler = handle_parent_signal;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
+	sa.sa_handler = SIG_IGN;
+	sigaction(SIGTERM, &sa, NULL);
 }
 
-void setup_child_signals(void)
+void	setup_child_signals(void)
 {
-    struct sigaction sa;
+	struct sigaction	sa;
 
-    sa.sa_handler = handle_child_signal;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    sigaction(SIGINT,  &sa, NULL);
-    sigaction(SIGQUIT, &sa, NULL);
-    sigaction(SIGTERM, &sa, NULL);
+	sa.sa_handler = handle_child_signal;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
+	sigaction(SIGTERM, &sa, NULL);
 }
