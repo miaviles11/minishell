a5b9b9d4686877c7b50d9ffe810f74b21905094d
CarlosVSL <carlosvsanchezlopez@gmail.com>
2025-05-29 18:23:01 +0200

    Norminette .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -18,6 +18,14 @@
 # include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+# include "../libft/libft.h"
+# include "parser.h"
+# include "redirections.h"
+# include "signals.h"
+# include "executor.h"
+
+# include "builtins.h"
 # define CWD_SIZE 1024
 
 typedef struct s_pipe
@@ -61,14 +69,6 @@ typedef struct s_msh
 	int		pipe;
 	int		redic;
 }				t_msh;
-
-# include "../libft/libft.h"
-# include "parser.h"
-# include "redirections.h"
-# include "signals.h"
-# include "executor.h"
-
-# include "builtins.h"
 
 /* clean_minishell.c */
 void	free_command_node(t_cmd *cmd);
